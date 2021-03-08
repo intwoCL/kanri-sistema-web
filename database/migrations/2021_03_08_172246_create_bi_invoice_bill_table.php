42ca2557a50a19167b7bf00eccf005e24683a9d0
Pablo Peña G <43855881+Justice2197@users.noreply.github.com>
2021-03-08 17:41:54 -0300

    create table invoice/bill

---
+++ database/migrations/2021_03_08_172246_create_bi_invoice_bill_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBiInvoiceBillTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bi_invoice_bill', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->references('id')->on('sys_company');
+            $table->foreignId('client_id')->references('id')->on('sys_client');
+            $table->foreignId('product_id')->references('id')->on('in_provider');
+            $table->datetime('issue_date');
+            $table->integer('quantity');
+            $table->double('price');
+            $table->double('subtotal');
+            $table->double('total');
+            $table->float('discount')->default(0);
+            $table->float('iva')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bi_invoice_bill');
+    }
+}
